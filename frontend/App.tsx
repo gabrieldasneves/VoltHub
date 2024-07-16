3318a235fd56a120f4e6f79f8fd4b934171efcda
gabrielalvesbrazil <125869083+gabrielalvesbrazil@users.noreply.github.com>
2024-07-16 10:12:17 +0900

    fix: give up on splash screen

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,32 +1,15 @@
-import React, { useState } from "react";
-import { Splash } from "./src/pages/Splash";
+import React from "react";
 import { StyleSheet } from "react-native";
-import AppLoading from "expo-app-loading";
 import { LinearGradient } from "expo-linear-gradient";
 import { Home } from "./src/pages/Home";
 
 export default function App() {
-  const [isReady, setIsReady] = useState(false);
-
-  const loadResources = async () => {
-    await new Promise((resolve) => setTimeout(resolve, 2000));
-  };
-
-  if (!isReady) {
-    return (
-      <AppLoading
-        startAsync={loadResources}
-        onFinish={() => setIsReady(true)}
-        onError={console.warn}
-      />
-    );
-  }
   return (
     <LinearGradient
       colors={["#151924", "#151924", "#464952"]}
       style={styles.linearGradient}
     >
-      {isReady ? <Home /> : <Splash />}
+      <Home />
     </LinearGradient>
   );
 }
